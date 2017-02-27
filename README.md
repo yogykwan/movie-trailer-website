31e8a1e8e77a84af3947bba13e4e139e1b8bb4cd
Jing Guan <jingg.cxy@foxmail.com>
2017-02-27 10:16:51 +0800

    review: coding style

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-Source code for a Movie Trailer website.
+# Movie Trailer Website
+A static web page allowing visitors to browse movies and watch trailers.Source code for a Movie Trailer website.
 
-# Run
-python entertainment_center.py
+## What’s included
+
+- Library fresh_tomatoes.py
+- Class Movie in media.py
+- Application entertainment_center.py
+
+## Usage
+
+- Open a terminal
+- Move to the project directory
+- Run `python entertainment_center.py`
--- media.py
+++ media.py
@@ -7,10 +7,12 @@ class Movie():
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
     
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        """Initialize Movie Class with title, storyline, poster, trailer"""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """Open browser to play movie trailers"""
         webbrowser.open(self.trailer_youtube_url)
